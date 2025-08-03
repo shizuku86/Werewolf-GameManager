43ec2bb44f7ad491eecb75f41882624cf86bbb30
shizuku86 <shizuku86710@gmail.com>
2025-08-04 00:49:24 +0900

    show addon name

--- src/utils/consoleManager.ts
+++ src/utils/consoleManager.ts
@@ -1,17 +1,15 @@
+import { properties } from "../properties";
+
 export class ConsoleManager {
     static log(message: string): void {
-        console.log(`[Werewolf][Log] ${message}`);
+        console.log(`${properties.header.name}[Log] ${message}`);
     }
 
     static warn(message: string): void {
-        console.warn(`[Werewolf][Warning] ${message}`);
+        console.warn(`${properties.header.name}[Warning] ${message}`);
     }
 
     static error(message: string): void {
-        console.error(`[Werewolf][Error] ${message}`);
-    }
-
-    static info(tag: string = "Info", message: string): void {
-        console.log(`[Werewolf][${tag}] ${message}`);
+        console.error(`${properties.header.name}[Error] ${message}`);
     }
 }
