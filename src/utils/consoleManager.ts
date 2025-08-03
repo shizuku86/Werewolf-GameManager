99bcf7f9125d865a9f114f32b26ce2d393f604d3
shizuku86 <shizuku86710@gmail.com>
2025-08-04 01:42:52 +0900

    fixed tags []

--- src/utils/consoleManager.ts
+++ src/utils/consoleManager.ts
@@ -2,14 +2,14 @@ import { properties } from "../properties";
 
 export class ConsoleManager {
     static log(message: string): void {
-        console.log(`${properties.header.name}[Log] ${message}`);
+        console.log(`[${properties.header.name}][Log] ${message}`);
     }
 
     static warn(message: string): void {
-        console.warn(`${properties.header.name}[Warning] ${message}`);
+        console.warn(`[${properties.header.name}][Warning] ${message}`);
     }
 
     static error(message: string): void {
-        console.error(`${properties.header.name}[Error] ${message}`);
+        console.error(`[${properties.header.name}][Error] ${message}`);
     }
 }
