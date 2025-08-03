0174fb98a4c45d0939e1578725c6b415fa74441b
shizuku86 <shizuku86710@gmail.com>
2025-08-04 01:43:02 +0900

    build

--- BP/scripts/utils/consoleManager.js
+++ BP/scripts/utils/consoleManager.js
@@ -1,12 +1,12 @@
 import { properties } from "../properties";
 export class ConsoleManager {
     static log(message) {
-        console.log(`${properties.header.name}[Log] ${message}`);
+        console.log(`[${properties.header.name}][Log] ${message}`);
     }
     static warn(message) {
-        console.warn(`${properties.header.name}[Warning] ${message}`);
+        console.warn(`[${properties.header.name}][Warning] ${message}`);
     }
     static error(message) {
-        console.error(`${properties.header.name}[Error] ${message}`);
+        console.error(`[${properties.header.name}][Error] ${message}`);
     }
 }
